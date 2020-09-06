a07c96e00afa368b2498cfb9581284819ca396a9
delma <sdy2856@gmail.com>
2020-09-07 00:39:04 +0900

    chore :tada: 프로젝트 생성

---
+++ RxSideDish/RxSideDish/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxSideDish
+//
+//  Created by Delma Song on 2020/09/07.
+//  Copyright © 2020 Delma Song. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
