9dfdff23e674a0719b87ca65c4485f30a40987fc
delma <sdy2856@gmail.com>
2020-09-18 17:49:56 +0900

    feat: MenuDetailViewController 생성 및 폴더 hierarchy 변경

---
+++ RxSideDish/RxSideDish/Controllers/MenuDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MenuDetailViewController.swift
+//  RxSideDish
+//
+//  Created by Delma Song on 2020/09/18.
+//  Copyright © 2020 Delma Song. All rights reserved.
+//
+
+import UIKit
+
+class MenuDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
