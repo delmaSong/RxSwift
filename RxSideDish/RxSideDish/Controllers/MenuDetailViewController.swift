6217638aba063c0664d7adfef878ef5ae5817a97
delma <sdy2856@gmail.com>
2020-09-19 18:04:38 +0900

    feat: UI 추가 및 IBOutlet 연결

--- RxSideDish/RxSideDish/Controllers/MenuDetailViewController.swift
+++ RxSideDish/RxSideDish/Controllers/MenuDetailViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class MenuDetailViewController: UIViewController {
 
+    @IBOutlet weak var thumbnailStackView: UIStackView!
+    @IBOutlet weak var detailStackView: UIStackView!
+    @IBOutlet weak var pointLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var deliveryFeeLabel: UILabel!
+    @IBOutlet weak var deliveryInfoLabel: UILabel!
+    @IBOutlet weak var originalPriceLabel: UILabel!
+    @IBOutlet weak var discountedPriceLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
